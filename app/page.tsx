8d78b62c62ee9df660a611278ca09f91ec69ee95
ShunDeveloper <s.tsuchiya.business@gmail.com>
2023-01-03 19:05:26 +0900

    making ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,74 @@
-import regix_dict from "../components/regix_dict";
+import '../styles/globals.css'
+import * as data from "../components/regix_dict";
 
 export default function Page() {
-    console.log(regix_dict)
-    return <div>Hello World!</div>
+    return (
+        <div>
+            <Title />
+            <div className="grid grid-cols-4 p-[3%]">
+                <Content />
+                <Sidebar />
+            </div>
+        </div>
+    )
+}
+
+const Title = () => {
+    return (
+        <div className='py-[2%] pl-[3%] bg-gray-800'>
+            <h1 className="text-gray-50 text-[30px]">
+                正規表現検索ページ
+            </h1>
+            <p className="text-gray-300">
+                正規表現をtagや文字列で検索することが出来るサイトです
+            </p>
+        </div>
+    )
+}
+
+const Content = () => {
+    return (
+        <main className='col-span-3'>
+            <h3>
+                検索結果
+            </h3>
+            <div>
+                {
+                    data.regix_list.map((value,key) => {
+                        return (
+                            <div key={key} className="rounded-lg py-[3%] pl-[3%] my-[1%] bg-gray-100 w-[80%]">
+                                <p>文法: {value.syntax}</p>
+                                <p>意味: {value.meaning}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </main>
+    )
+}
+
+const Sidebar = () => {
+    return (
+        <div>
+            <h3>Tag 検索</h3>
+            <Tags />
+        </div>
+    )
+}
+
+const Tags = () => {
+    return (
+        <ul>
+            {
+                data.tags.map((value,key) => {
+                    return (
+                        <li key={key}>
+                            {value}
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    )
 }
--- components/regix_dict.tsx
+++ components/regix_dict.tsx
@@ -4,17 +4,47 @@ type regix_data = {
     meaning: string
 };
 
-const regix_list: regix_data[] = [
+export const regix_list: regix_data[] = [
     {
         syntax: '.',
-        tag: ['1文字', '一文字'],
+        tag: ['1文字', '一文字','任意'],
         meaning: '任意の1文字'
     },
     {
         syntax: '[abc]',
-        tag: ['1文字', '一文字'],
+        tag: ['1文字', '一文字','指定'],
         meaning: '指定した1文字(この場合, aまたはbまたはcを指定している)'
+    },
+    {
+        syntax: '[^abc]',
+        tag: ['1文字', '一文字','否定'],
+        meaning: '指定した文字以外の1文字(この場合, aまたはbまたはc以外を指定している)'
+    },
+    {
+        syntax: 'back slash + meta',
+        tag: ['メタ文字', '一文字'],
+        meaning: '指定したメタ文字一文字'
+    },
+    {
+        syntax: 'word1 | word2',
+        tag: ['任意', '単語'],
+        meaning: '指定した単語'
+    },
+    {
+        syntax: 'word1 | word2',
+        tag: ['任意', '単語'],
+        meaning: '指定した単語のいずれか'
     },
 ]
 
-export default regix_list
+const str_arr_uniq= (string_array: string[]) =>  {
+    return Array.from(new Set(string_array));
+}
+
+let tags_tmp: string[] = []
+regix_list.map((regix_i, index, object) => {
+    regix_i.tag.map((tag_ij, index2, object2) => {
+        tags_tmp.push(tag_ij)
+    })
+});
+export const tags = str_arr_uniq(tags_tmp)
