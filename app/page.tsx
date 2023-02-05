5e26d46bcc824ee784f6555d50ca7d0ac2e8ddd6
ShunDeveloper <s.tsuchiya.business@gmail.com>
2023-02-05 14:53:58 +0900

    queryを取得できるようにしました

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
+'use client';
+
 import '../styles/globals.css'
 import choose_show_regex_list from '../libs/choose_show_regex_list';
 import create_unique_tag_list_from_regex_list from '../libs/create_tag_list_from_regex_data';
 import regex_data from '../libs/type_components';
+import Link from 'next/link'
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from "next/navigation";
+import {useState, useEffect} from 'react'
+
 
 export default function Page() {
+    const [chooseTags, setChooseTags] = useState('')
+    const searchParams = useSearchParams()
     const regex_list = choose_show_regex_list(['basic'])
     const tags = create_unique_tag_list_from_regex_list(regex_list)
+    const onClickLi = (value: string) => {}
+    useEffect(() => {
+        // Update the document title using the browser API
+        console.log(searchParams.get('tag'))
+        const tmp = searchParams.get('tag')
+        if (null !== tmp) {
+            setChooseTags(tmp)
+            console.log(chooseTags)
+        }
+    });
  
     return (
         <div className="MainContainer">
@@ -14,6 +33,7 @@ export default function Page() {
             />
             <Sidebar
                 tags = {tags}
+                func = {(value: string) => onClickLi(value)}
             />
         </div>
     )
@@ -21,50 +41,62 @@ export default function Page() {
 
 const Content = (props: {data: regex_data[]}) => {
     return (
-        <main className='col-span-3'>
+        <div className='col-span-3'>
             <h3>
                 検索結果
             </h3>
-            <div>
-                {
-                    props.data.map((value,key) => {
-                        return (
-                            <div key={key} className="rounded-lg py-[3%] pl-[3%] my-[1%] bg-gray-100 w-[80%]">
-                                <p>文法: {value.syntax}</p>
-                                <p>意味: {value.meaning}</p>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </main>
-    )
-}
-
-const Sidebar = (props: {tags: string[]}) => {
-    return (
-        <div>
-            <h3>Common Tags</h3>
-            <Tags
-                tags = {['文法','メタ','応用']}
-            />
-            <h3>ALL Tags</h3>
-            <Tags
-                tags = {props.tags}
+            <ShowData
+                data = {props.data}
             />
         </div>
     )
 }
 
-const Tags = (props: {tags: string[]}) => {
+const ShowData = (props: {data: regex_data[]}) => {
+    return (
+        <div>
+            {
+                props.data.map((value,key) => {
+                    return (
+                        <div key={key} className="Card">
+                            <p>文法: {value.syntax}</p>
+                            <p>意味: {value.meaning}</p>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+const Sidebar = (props: {tags: string[], func: Function}) => {
+    return (
+        <div>
+            <h3>Common Tags</h3>
+            <Tags
+                tags = {['基本文法','メタ文字','応用']}
+                func = {props.func}
+            />
+            <h3>ALL Tags</h3>
+            <Tags
+                tags = {props.tags}
+                func = {(value:string) => props.func(value)}
+            />
+        </div>
+    )
+}
+
+const Tags = (props: {tags: string[], func: Function}) => {
     return (
         <ul>
             {
                 props.tags.map((value,key) => {
                     return (
-                        <li key={key}>
-                            {value}
-                        </li>
+                        <Link href ={"/?tag="+value} key={key}>
+                            <li onClick={(e) => props.func(e.currentTarget)}>
+                                {value}
+                            </li>
+                        </Link>
                     )
                 })
             }
